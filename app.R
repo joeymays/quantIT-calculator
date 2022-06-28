05457f820333f53d83a143e71cb6d7e1ba0d6337
Joey Mays <joeymays94@gmail.com>
2022-06-28 19:43:37 -0400

    basic inputs

--- app.R
+++ app.R
@@ -3,27 +3,17 @@ library(shinyMatrix)
 
 ui <- fluidPage(
   
-  # Application title
   titlePanel("QuantIT Calculator"),
   
-  # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
       
       textInput("pasteinput", label = "paste here", placeholder = "paste spreadsheet here"),
       
-      
-      
-      matrixInput(inputId = "inputmat", 
-                  value = matrix(data = 0, 
-                                 nrow = 8, ncol = 12, 
-                                 dimnames = list(LETTERS[1:8], 1:12))
-                  ),
-      actionButton("simulate", "Simulate!"),
-      
+      selectInput(inputId = "standardcolumn", "Standard Column", selected = 1, choices = 1:12)
+
     ),
     
-    # Show a plot of the generated distribution
     mainPanel(
       tableOutput("obs")
     )
@@ -31,17 +21,29 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
-
-  pasteInput <- reactive({
-    raw.paste <- input$pasteinput
-    y <- strsplit(raw.paste, "\n")
-    z <- strsplit(y[[1]], "\t")
-    paste.neat <- matrix(unlist(z), nrow = 8, byrow = T)
-    return(paste.neat)
-  })
-  
-  output$obs <- renderTable({pasteInput()})
-
+    
+    pasteInput <- reactive({
+        req(input$pasteinput)
+        raw.paste <- input$pasteinput
+        y <- strsplit(raw.paste, "\\s")[[1]]
+        
+        if(length(y) < 96){
+            y <- c(y, rep("", 96 - length(y)))
+        }
+        
+        y[which(y == "")] <- NA
+        paste.neat <- matrix(as.numeric(y), nrow = 8, byrow = T)
+        return(paste.neat)
+    })
+    
+    pastePreview <- reactive({
+        round(pasteInput(), 1)
+    })
+    
+    output$obs <- renderTable({
+        req(input$pasteinput)
+        pastePreview()})
+    
 }
 
 shinyApp(ui = ui, server = server)
